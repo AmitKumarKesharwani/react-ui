c117108b12228ba4d128e61e2a2f97866caf7360
Amit Kumar <amitkumarkesharwani98@gmail.com>
2025-04-29 22:34:40 +0530

    Minor Updates.

--- src/App.js
+++ src/App.js
@@ -3,7 +3,14 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      Amit
+        <div className=''>
+          <h1>Header</h1>
+        </div>
+
+        <div className='d-flex gap-1.5'>
+          <span className='w-1/2'>One</span>
+          <span className='w-1/2'>One 1</span>
+        </div>
     </div>
   );
 }
